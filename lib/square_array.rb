4d08d81741ae0899ba316cbbaaa846057c2edde8
Seohee  Im <seoheeim@vt.edu>
2020-07-22 11:30:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,5 @@
 def square_array(numbers) 
   length = numbers.length   
-  while length.times do |number|
-    puts array[number] * array[number]
+  while length.times do 
+    puts numbers[length]
 end
